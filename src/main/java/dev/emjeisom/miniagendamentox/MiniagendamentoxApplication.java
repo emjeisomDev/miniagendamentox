f8dc481e933384686940bdd9b2cd979bcda94e47
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-01-28 15:00:21 -0300

    Project Created

---
+++ src/main/java/dev/emjeisom/miniagendamentox/MiniagendamentoxApplication.java
@@ -0,0 +1,13 @@
+package dev.emjeisom.miniagendamentox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniagendamentoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniagendamentoxApplication.class, args);
+	}
+
+}
