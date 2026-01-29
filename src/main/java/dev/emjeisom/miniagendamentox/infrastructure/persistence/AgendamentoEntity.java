dadd1ef1fc3b8a51374545e178efb30cb9005869
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-01-29 14:32:14 -0300

    Created AgendamentoEntity in Infraestrutura package.

---
+++ src/main/java/dev/emjeisom/miniagendamentox/infrastructure/persistence/AgendamentoEntity.java
@@ -0,0 +1,45 @@
+package dev.emjeisom.miniagendamentox.infrastructure.persistence;
+
+import dev.emjeisom.miniagendamentox.core.enums.StatusAgendamento;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="tb_Agendamento")
+@Builder
+public class AgendamentoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false, length = 120)
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    @Column(name = "data_inicio", nullable = false)
+    private LocalDateTime dataInicio;
+
+    @Column(name = "data_fim", nullable = false)
+    private LocalDateTime dataFim;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private StatusAgendamento status;
+
+    @Column(nullable = false, length = 80)
+    private String usuario;
+
+    @Column(name="criado_em", nullable = false)
+    private LocalDateTime criadoEm;
+
+    @Column(name="atualizado_em", nullable = false)
+    private LocalDateTime atualizadoEm;
+}
---
+++ src/main/java/dev/emjeisom/miniagendamentox/infrastructure/persistence/AgendamentoRepository.java
@@ -0,0 +1,29 @@
+package dev.emjeisom.miniagendamentox.infrastructure.persistence;
+
+import dev.emjeisom.miniagendamentox.core.entities.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+    @Query(
+            """
+            SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END
+                FROM AgendamentoEntity a
+                WHERE a.usuario = :usuario
+                    AND a.status = dev.emjeisom.miniagendamentox.core.enums.StatusAgendamento.AGENDADO
+                    AND (a.dataInicio < :fim AND a.dataFim > :inicio)
+                    AND (:ignoreId IS NULL OR a.id <> :ignoreId)
+            """
+    )
+    boolean existsConflito(@Param("usuario") String usuario,
+                           @Param("inicio") LocalDateTime inicio,
+                           @Param("fim") LocalDateTime fim,
+                           @Param("ignoreId") Long ignoreId);
+
+}
